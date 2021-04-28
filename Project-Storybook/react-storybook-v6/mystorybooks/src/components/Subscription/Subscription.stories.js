0d0ccda845c8c70af70d9ca3032132f81d38e227
Hamdamboy <hamdamboy.urunov@gmail.com>
2021-04-28 18:30:21 +0900

    React Storybook

--- Project-Storybook/react-storybook-v6/mystorybooks/src/components/Subscription/Subscription.stories.js
+++ Project-Storybook/react-storybook-v6/mystorybooks/src/components/Subscription/Subscription.stories.js
@@ -2,7 +2,6 @@ import React from "react";
 import {Primary} from "../Button/Button.stories";
 import {Large} from "../Input/Input.stories";
 import Center from "../Center/Center";
-
 export default {
     title: "form/Subscription",
 }
